187bbff03cb07472231d85e22fe096d9c7f905fe
Avery Myers <averyjmyers@hotmail.com>
2023-08-09 13:27:41 -0700

    user model

---
+++ Develop/models/user.js
@@ -0,0 +1,69 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+
+class User extends Model {
+    checkPassword(){
+        return bcrypt.compareSync(loginPw, this.password)
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [0,30],
+            },
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [0,30],
+            },
+        },
+        email: {
+            type: DataTypes.STIMG,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5],
+            },
+        },
+    },
+    {
+        hooks: {
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+          beforeUpdate: async (updatedUserData) => {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored:true,
+        modelName: 'user',
+    },
+);
+
+module.exports = User;
