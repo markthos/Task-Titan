5bd5e5ed42cd34a5a7c81d27467b0a7e84babc5c
unknown <arundvp@gmail.com>
2023-08-09 19:57:23 -0700

    added routes

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,1 +1,9 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const projectRoutes = require('./projectRoutes');
+
+router.use('/users', userRoutes);
+router.use('/projects', projectRoutes);
+
+module.exports = router;
 
