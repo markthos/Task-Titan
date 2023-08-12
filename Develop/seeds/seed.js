25fe9bab83fca6b91c644ede4ac3c47b40f7401f
Avery Myers <averyjmyers@hotmail.com>
2023-08-12 13:00:06 -0700

    updated models and seeds for Comments

--- Develop/seeds/seed.js
+++ Develop/seeds/seed.js
@@ -1,9 +1,39 @@
 const sequelize = require('../config/connection');
-const { User, Project, Ticket } = require('../models');
+const { User, Project, Ticket, Comment } = require('../models');
 const userData = require('./userData.json');
 const projectData = require('./projectData.json');
 const ticketData = require('./ticketData.json')
+const commentData = require('./commentData.json')
 
+
+
+
+const seedDatabase = async () => {
+  await sequelize.sync ({ force: true })
+  try{
+    const users = [];
+    for (const userDataItem of userData) {
+      const user = await User.create(userDataItem);
+      users.push(user);
+    }
+    const projects = [];
+        for (const projectDataItem of projectData) {
+            const project = await Project.create(projectDataItem);
+            projects.push(project);
+        }
+    await Ticket.bulkCreate(ticketData);
+    await Comment.bulkCreate(commentData)
+
+    console.log('DB seeded successfully!')
+  } catch (error){
+    console.error('Error seeding db', error)
+  } finally {
+    await sequelize.close()
+  }
+}
+
+seedDatabase()
+/*
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -39,3 +69,4 @@ const seedDatabase = async () => {
   };
 
 seedDatabase()
+*/
