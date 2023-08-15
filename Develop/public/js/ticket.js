d4fafefd42c9e9d43b1dc07eef779c151c354527
1willcobb <cobb.will@gmail.com>
2023-08-14 21:13:27 -0700

    added ticket.js stuff

--- Develop/public/js/ticket.js
+++ Develop/public/js/ticket.js
@@ -1,183 +1,186 @@
-const deleteBlog = async (id, card) => {
-    console.log('update Blog Function... ' + id);
-  
-    const response = await fetch(`/api/blog/${id}`, {
-      method: 'DELETE',
-      body: JSON.stringify({ id }),
+const deleteTicket = async (id, ticket) => {
+  console.log("deleting ticket...");
+
+  const response = await fetch(`/api/ticket/${id}`, {
+    method: "DELETE",
+    body: JSON.stringify({ id }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    ticket.remove();
+  } else {
+    return console.log("nothing to delete");
+  }
+
+  location.reload();
+};
+
+const openEditor = async (id) => {
+  console.log("editing ticket...");
+
+  await fetch(`/api/ticket/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ is_editing: true }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  location.reload();
+};
+
+const handleUpdateSubmit = async (id, title, text) => {
+  console.log("saving ticket...");
+
+  if (title && text) {
+    const response = await fetch(`/api/ticket/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ title, text, is_editing: false }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
-  
+
     if (response.ok) {
-      card.remove();
+      location.reload();
     } else {
-      return console.log('nothing to delete');
+      alert("Failed to update");
     }
-  
-    location.reload();
-  };
-  
-  const openEditor = async (id) => {
-    console.log('editing...');
-  
-    await fetch(`/api/blog/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({ is_editing: true }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  
-    location.reload();
-  };
-  
-  const handleUpdateSubmit = async (id, title, text) => {
-    console.log('saving...');
-  
-    if (title && text) {
-      const response = await fetch(`/api/blog/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({ title, text, is_editing: false }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        location.reload();
-      } else {
-        alert('Failed to update');
-      }
-    }
-  };
-  
-  const handleCancel = async (id) => {
-    console.log('editing...');
-  
-    await fetch(`/api/blog/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({ is_editing: false }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  
-    location.reload();
-  };
-  
-  const openComments = async (id, card) => {
-    const commentList = card.querySelector('#comment_list');
-    console.log('opening comments for blog id: ', id);
-    commentList.innerHTML = '';
-  
-    const response = await fetch(`/api/blog/${id}/comments`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  
-    const commentsData = await response.json(); // Parse JSON response
-  
-    const length = commentsData.comments.length;
-  
-    if (length <= 0) {
-      commentList.innerHTML = 'No Comments Yet';
-    }
-  
-    for (let i = 0; i < commentsData.comments.length; i++) {
-      commentList.innerHTML += `
+  }
+};
+
+const handleCancel = async (id) => {
+  console.log("Canceling update ticket...");
+
+  await fetch(`/api/ticket/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ is_editing: false }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  location.reload();
+};
+
+const openComments = async (id, ticket) => {
+  const commentList = ticket.querySelector("#comment_list");
+  console.log("opening comments for ticket id: ", id);
+  commentList.innerHTML = "";
+
+  const response = await fetch(`/api/ticket/${id}/comments`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const commentsData = await response.json(); // Parse JSON response
+
+  const length = commentsData.comments.length;
+
+  if (length <= 0) {
+    commentList.innerHTML = "No Comments Yet";
+  }
+
+  for (let i = 0; i < commentsData.comments.length; i++) {
+    commentList.innerHTML += `
       <li style="width: 100%; display: flex; justify-content: space-between; margin: 10px 0px;">
         <div>
           ${commentsData.comments[i].text}
         </div>
         <div>
           ${commentsData.comments[i].user.username} on ${dayjs(
-            commentsData.comments[i].createdAt,
-          ).format('MM/DD/YYYY')}
+            commentsData.comments[i].createdAt
+          ).format("MM/DD/YYYY")}
         </div
       </li>`;
-    }
-  };
-  
-  const handleNewCommentSubmit = async (blog_id, card) => {
-    console.log('submitting new comment...');
-  
-    const textField = card.querySelector('#new_comment');
-  
-    const text = textField.value;
-  
-    console.log(text);
-    console.log(blog_id);
-  
-    const response = await fetch('/api/comment', {
-      method: 'POST',
-      body: JSON.stringify({ text, blog_id }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  
-    openComments(blog_id, card);
-  
-    if (!response.ok) {
-      console.log('something went wrong');
-    }
-  };
-  
-  document.addEventListener('click', async (event) => {
-    const target = event.target;
-    const card = target.closest('.card'); // Find the closest parent with the .card class
-  
-    if (!card) {
-      return; // Click didn't happen within a card, exit
-    }
-  
-    const blogId = card.getAttribute('data-id');
-    console.log('here ' + blogId);
-  
-    if (target.matches('.comment-button')) {
-      // Handle comment button click
-      const commentState = target.getAttribute('data-state');
-  
-      const commentSection = card.querySelector('#comments_section');
-      if (commentState === 'closed') {
-        target.setAttribute('data-state', 'open');
-        target.innerHTML = 'Hide Comments';
-        commentSection.style.display = 'flex';
-        openComments(blogId, card);
-      } else {
-        target.innerHTML = 'Comments';
-        commentSection.style.display = 'none';
-        target.setAttribute('data-state', 'closed');
-      }
-    } else if (target.matches('#add_comment')) {
-      // Handle add comment button click
-      const addCommentDisplay = card.querySelector('#add_comment_display');
-      if (addCommentDisplay.style.display === 'none') {
-        target.innerHTML = 'Cancel';
-        addCommentDisplay.style.display = 'block';
-      } else {
-        target.innerHTML = 'Add Comment +';
-        addCommentDisplay.style.display = 'none';
-      }
-    } else if (target.matches('#submit_new_comment')) {
-      // Handle submit new comment button click
-      handleNewCommentSubmit(blogId, card);
-    } else if (target.matches('.edit_button')) {
-      openEditor(blogId);
-    } else if (target.matches('.delete_button')) {
-      deleteBlog(blogId, target);
-    } else if (target.matches('.edit_button')) {
-      openEditor(blogId);
-    } else if (target.matches('.save_button')) {
-      const title = card.querySelector('#new_blog_title').value.trim();
-      const text = card.querySelector('#new_blog_content').value.trim();
-      handleUpdateSubmit(blogId, title, text);
-    } else if (target.matches('.cancel_button')) {
-      console.log("here")
-      handleCancel(blogId, card);
-    }
+  }
+};
+
+const handleNewCommentSubmit = async (ticket_id, ticket) => {
+  console.log("submitting new comment...");
+
+  const textField = ticket.querySelector("#new_comment");
+
+  const text = textField.value;
+
+  console.log(text);
+  console.log(ticket_id);
+
+  const response = await fetch("/api/comment", {
+    method: "POST",
+    body: JSON.stringify({ text, ticket_id }),
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
+
+  openComments(ticket_id, ticket);
+
+  if (!response.ok) {
+    console.log("something went wrong");
+  }
+};
+
+const main = document.querySelector('#main');
+
+document.addEventListener("click", async (event) => {
+  const target = event.target;
+  console.log(target)
+  const ticket = target.closest(".ticket"); // Find the closest parent with the .ticket class
+  console.log("hey")
+  if (!ticket) {
+    return; // Click didn't happen within a ticket, exit
+  }
+
+  const ticketId = ticket.getAttribute("data-id");
   
+
+  if (target.matches(".comment-button")) {
+    // Handle comment button click
+    const commentState = target.getAttribute("data-state");
+
+    const commentSection = ticket.querySelector("#comments_section");
+    if (commentState === "closed") {
+      target.setAttribute("data-state", "open");
+      target.innerHTML = "Hide Comments";
+      commentSection.style.display = "flex";
+      openComments(ticketId, ticket);
+    } else {
+      target.innerHTML = "Comments";
+      commentSection.style.display = "none";
+      target.setAttribute("data-state", "closed");
+    }
+  } else if (target.matches("#add_comment")) {
+    // Handle add comment button click
+    const addCommentDisplay = ticket.querySelector("#add_comment_display");
+    if (addCommentDisplay.style.display === "none") {
+      target.innerHTML = "Cancel";
+      addCommentDisplay.style.display = "flex";
+    } else {
+      target.innerHTML = "Add Comment +";
+      addCommentDisplay.style.display = "none";
+    }
+  } else if (target.matches("#submit_new_comment")) {
+    // Handle submit new comment button click
+    handleNewCommentSubmit(ticketId, ticket);
+  } else if (target.matches(".edit_button")) {
+    openEditor(ticketId);
+  } else if (target.matches(".delete_button")) {
+    deleteBlog(ticketId, target);
+  } else if (target.matches(".edit_button")) {
+    openEditor(ticketId);
+  } else if (target.matches(".save_button")) {
+    const title = ticket.querySelector("#new_ticket_title").value.trim();
+    const text = ticket.querySelector("#new_ticket_content").value.trim();
+    handleUpdateSubmit(ticketId, title, text);
+  } else if (target.matches(".cancel_button")) {
+    console.log("here");
+    handleCancel(ticketId, ticket);
+  }
+});
+
--- Develop/utils/helpers.js
+++ Develop/utils/helpers.js
@@ -1,9 +1,15 @@
+const dayjs = require("dayjs");
+
 const compare = (variableOne, comparator, variableTwo) => {
-      if (eval(one + comparator + two)) {
-        return true
-      } else {
-        return false
-      }
+  if (eval(one + comparator + two)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-module.exports = {compare}
+const format_date = (date) => {
+  return dayjs(date).format("MM/DD/YYYY");
+};
+
+module.exports = { compare, format_date };
