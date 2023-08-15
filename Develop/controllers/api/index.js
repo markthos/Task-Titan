26720edb1742d51afae62b9d5755b14ee4a374b1
unknown <arundvp@gmail.com>
2023-08-14 20:49:43 -0700

    added ticketRoutes and verified it!

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
+const ticketsRoutes = require('./ticketsRoutes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
+router.use('/tickets', ticketsRoutes);
 
 module.exports = router;
 
