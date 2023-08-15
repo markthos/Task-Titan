47573a42982933ab46359cf82004da3d9d07269d
Avery Myers <averyjmyers@hotmail.com>
2023-08-15 15:05:05 -0700

    updated relationships for through table

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,18 +4,33 @@ const Ticket = require('./ticket')
 const Comment = require('./comment');
 const Collaborator = require('./collaborator');
 
+const User = require('./user');
+const Project = require('./project');
+const Ticket = require('./ticket')
+const Comment = require('./comment');
+const Collaborator = require('./collaborator');
+
 User.belongsToMany(Project, {
   through:{
       model: Collaborator,
       foreignKey: 'user_id',
     },
   onDelete: 'CASCADE',
+  as: 'collaborations'
+});
+
+Project.belongsToMany(User, {
+  through: {
+    model: Collaborator,
+    foreignKey: 'project_id',
+  },
+  onDelete: 'CASCADE',
+  as: 'collaborators'
 });
 
 Project.belongsTo(User, {
   foreignKey: 'owner_id' // Will changed this, is it right? it was user_id
 });
-
 
 Project.hasMany(Ticket, {
   foreignKey: 'project_id'
@@ -37,18 +52,14 @@ Comment.belongsTo(Project,{
   foreignKey: 'project_id'
 });
 
-Collaborator.hasMany(Collaborator,{
-  foreignKey: 'project_id'
+Collaborator.belongsTo(User,{
+  foreignKey: 'user_id'
 })
 
 Collaborator.belongsTo(Project,{
   foreignKey: 'project_id'
 });
 
-
-
-
-
-
 module.exports = { User, Project, Ticket, Comment, Collaborator };
+
 
