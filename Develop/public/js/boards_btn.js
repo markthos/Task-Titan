b4aacfdae4c42195f72a3306b016a1c64a314e85
Mark T <aydenthos@gmail.com>
2023-08-17 23:12:04 -0700

    modal functioning

--- Develop/public/js/boards_btn.js
+++ Develop/public/js/boards_btn.js
@@ -3,6 +3,13 @@ var modal = document.getElementById("myModal");
 // Get the modal
 var modal2 = document.getElementById("myModal2");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// When the user clicks on the button, open the modal
+btn.addEventListener("click", function(event) {
+  event.preventDefault();
+  modal.style.display = "block";
+});
 
 // Get the button that opens the modal2
 var btn2 = document.getElementById("myBtn2");
@@ -13,22 +20,29 @@ btn2.addEventListener("click", function(event) {
   modal2.style.display = "block";
 });
 
-// Get the <span> element that closes modals
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks on <span> (x), closes modals
-span.addEventListener("click", function(event) {
-  event.preventDefault();
-  modal.style.display = "none";
-  modal2.style.display = "none";
+// When the user clicks anywhere outside of the modals, close them
+window.addEventListener("click", function(event) {
+  console.log(event.target);
+  if (event.target == modal || event.target == modal2) {
+    console.log("hello");
+    modal.style.display = "none";
+    modal2.style.display = "none";
+  }
 });
 
-
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// When the user clicks on the button, open the modal
-btn.addEventListener("click", function(event) {
+// Get the <span> element that closes modal
+var spanModal = document.getElementsByClassName("close")[1];
+// When the user clicks on <span> (x), closes modal
+spanModal.addEventListener("click", function(event) {
   event.preventDefault();
-  modal.style.display = "block";
+  modal.style.display = "none";
+});
+
+// Get the <span> element that closes modal2
+var spanModal2 = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), closes modal2
+spanModal2.addEventListener("click", function(event) {
+  event.preventDefault();
+  modal2.style.display = "none";
 });
 
